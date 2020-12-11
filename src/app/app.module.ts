e676dd2f53345050d8af8c09b9382d159a5bac83
ando8841 <oleshchuk91@bk.ru>
2020-12-11 22:00:47 +0300

    added texts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import {MatIconModule} from '@angular/material/icon';
 import { ServiceComponent } from './widgets/service/service.component';
 import { ShortComponent } from './widgets/short/short.component';
 import { ExamplesComponent } from './widgets/examples/examples.component';
+import { FooterComponent } from './widgets/footer/footer.component';
+import { MenuComponent } from './widgets/menu/menu.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @NgModule({
   declarations: [
@@ -16,13 +22,18 @@ import { ExamplesComponent } from './widgets/examples/examples.component';
     BenefitsComponent,
     ServiceComponent,
     ShortComponent,
-    ExamplesComponent
+    ExamplesComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
