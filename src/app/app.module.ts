b0459eb958736b2a314337bb70e6e5d8ba57ccfe
ando8841 <oleshchuk91@bk.ru>
2020-12-11 21:03:06 +0300

    added  4 components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BenefitsComponent } from './widgets/benefits/benefits.component';
+import {MatIconModule} from '@angular/material/icon';
+import { ServiceComponent } from './widgets/service/service.component';
+import { ShortComponent } from './widgets/short/short.component';
+import { ExamplesComponent } from './widgets/examples/examples.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BenefitsComponent,
+    ServiceComponent,
+    ShortComponent,
+    ExamplesComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
